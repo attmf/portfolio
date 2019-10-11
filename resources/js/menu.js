9080b49e8ac49e418821d66089c9d04fc5b4397e
Anderson Maronato <anderson.maronato@Office-Fulano>
2019-10-11 17:12:02 -0300

    criado menu e animação do menu mobile, crud mobile arrumado, criado form de contato

---
+++ resources/js/menu.js
@@ -0,0 +1,34 @@
+let btnHamb = document.getElementById('hamburguer');
+let menuLateral = document.getElementById('lateral-menu');
+let navLink = document.querySelectorAll('.nav-link');
+let click = false;
+
+// document.addEventListener("click", function(e) {
+//     if (!menuLateral == e.target && !btnHamb == e.target && click == true) {
+//         menuLateral.classList.remove('anim');
+//         menuLateral.classList.remove('open');
+//         click = false;
+//         console.log('oi hide');
+//     }
+//     if(navLink == e.target){
+//         menuLateral.classList.remove('anim');
+//         menuLateral.classList.remove('open');
+//         click = false;
+//     }
+// });
+
+btnHamb.addEventListener("click", function(e) {
+    if(click == false){
+        menuLateral.classList.add('anim');
+        btnHamb.classList.add('open');
+        click = true;
+        console.log('oi show');
+    }
+    else{
+        menuLateral.classList.remove('anim');
+        btnHamb.classList.remove('open');
+        click = false;
+    }
+});
+
+
--- resources/js/scroll.js
+++ resources/js/scroll.js
@@ -1,19 +1,60 @@
-jQuery(function () {
-    jQuery(window).scroll(function () {
-        if (jQuery(this).scrollTop() < 600) {
-            $("#home-link").addClass("active");
-            $("#about-link").removeClass("active");
-            $("#projects-link").removeClass("active");
-            $("#contact-link").removeClass("active");
-        } else if (jQuery(this).scrollTop() > 600) {
-            $("#home-link").removeClass("active");
-            $("#about-link").addClass("active");
-        } else if (jQuery(this).scrollTop() > 800) {
-            $("#about-link").removeClass("active");
-            $("#projects-link").addClass("active");
-        } else if (jQuery(this).scrollTop() > 1800) {
-            $("#projects-link").removeClass("active");
-            $("#contact-link").addClass("active")
-        }
+// jQuery(function () {
+//     jQuery(window).scroll(function () {
+//         if (jQuery(this).scrollTop() < 650) {
+//             $("#home-link").addClass("active");
+//             $("#about-link").removeClass("active");
+//             $("#projects-link").removeClass("active");
+//             $("#contact-link").removeClass("active");
+//         } else if (jQuery(this).scrollTop() > 1800) {
+//             $("#projects-link").removeClass("active");
+//             $("#contact-link").addClass("active")
+//         } else if (jQuery(this).scrollTop() > 1200) {
+//             $("#about-link").removeClass("active");
+//             $("#projects-link").addClass("active");
+//         } else if (jQuery(this).scrollTop() > 650) {
+//             $("#home-link").removeClass("active");
+//             $("#about-link").addClass("active");
+//         }
+//     });
+// });
+
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
     });
 });
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#lateral-menu a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#lateral-menu a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
