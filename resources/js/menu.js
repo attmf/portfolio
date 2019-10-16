a242bc29d391e60d0b8766f41dfd50279153275a
attmf <anderson.maronato@etec.sp.gov.br>
2019-10-16 17:38:07 -0300

    inicio do layout dos jobs-resumes de cada job-card

--- resources/js/menu.js
+++ resources/js/menu.js
@@ -1,7 +1,12 @@
 let btnHamb = document.getElementById('hamburguer');
 let menuLateral = document.getElementById('lateral-menu');
 let navLink = document.querySelectorAll('.nav-link');
+let jobCard = document.querySelectorAll('.job-card');
+let jobResume = document.getElementsByClassName('job-resume');
+let bgFocus = document.getElementsByClassName('.job-resume');
+let btnClose = document.getElementById('btnClose');
 let click = false;
+let jobCardFocus;
 
 // document.addEventListener("click", function(e) {
 //     if (!menuLateral == e.target && !btnHamb == e.target && click == true) {
@@ -23,7 +28,20 @@ for(i = 0; i < navLink.length;i++) {
         btnHamb.classList.remove('open');
         click = false;
     });
-  }
+}
+
+for(i = 0; i < jobCard.length;i++) {
+    jobCard[i].addEventListener('click', function(){
+        this.classList.add('job-card-active');
+        jobCardFocus = this;
+        jobResume[0].classList.add('d-block');
+    });
+}
+
+btnClose.addEventListener("click", function(e) {
+    jobResume[0].classList.remove('d-block');
+    jobCardFocus.classList.remove('job-card-active');
+});
 
 btnHamb.addEventListener("click", function(e) {
     if(click == false){
